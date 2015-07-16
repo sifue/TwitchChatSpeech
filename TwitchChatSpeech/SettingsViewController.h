c31a1e816b4e5e5c51b80e754a312b56e2a00c6d
sifue <yoshimura@soichiro.org>
2015-07-16 17:49:38 +0900

    設定画面を含めたレイアウトを作るところまで

--- TwitchChatSpeech/SettingsViewController.h
+++ TwitchChatSpeech/SettingsViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  SettingsViewController.h
 //  TwitchChatSpeech
 //
 //  Created by 吉村 総一郎 on 2015/07/16.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface SettingsViewController : UIViewController
 
 @end
-
